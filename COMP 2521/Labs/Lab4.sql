0e7cf444fc4a87e3b9206c59372940d4936b2e88
BetoDuque6347 <gduqu594@mtroyal.ca>
2026-03-09 12:53:22 -0600

    Added Lab4.sql and its instructions.

---
+++ COMP 2521/Labs/Lab4.sql
@@ -0,0 +1,9 @@
+-- Lab 4
+-- Author: Beto Duque
+-- Instructor: Shoba Ittyipe
+-- Due date: Mar 9, 2026
+
+--Retrieve the locations to show the number of course sections scheduled to run in each of those
+--locations. Sort the result by location ID. Include also locations that do not have a course section
+--scheduled in them yet.
+
