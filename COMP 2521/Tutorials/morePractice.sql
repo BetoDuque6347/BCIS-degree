e631f28f1b5ccac7c907b913bcaf51749e1be1c9
BetoDuque6347 <gduqu594@mtroyal.ca>
2026-04-15 19:03:37 -0600

    Did some subquery practice

--- COMP 2521/Tutorials/morePractice.sql
+++ COMP 2521/Tutorials/morePractice.sql
@@ -109,3 +109,85 @@ WHERE s_id IN (
 -- Second set
 
 -- Q1.
+SELECT s_id
+FROM student
+WHERE s_id = MIN(s_id);
+GROUP BY s_id
+
+-- Q2.
+SELECT c_sec_id
+FROM course_section
+WHERE loc_id IN (
+    SELECT loc_id
+    FROM location
+    WHERE bldg_code = 'CR'
+);
+
+-- Q3.
+SELECT s_id
+FROM students
+WHERE s_id NOT IN (
+    SELECT s_id
+    FROM enrollment
+    WHERE term_id = 1
+);
+
+-- Q4.
+SELECT c_sec_id
+FROM course_section
+WHERE max_enrl > (
+    SELECT AVG(max_enrl)
+    FROM course_section
+)
+GROUP BY max_enrl;
+
+-- Q5.
+SELECT s_id
+FROM student
+WHERE s_id IN (
+    SELECT s_id
+    FROM enrollment
+    WHERE c_sec_id IN (
+        SELECT c_sec_id
+        FROM course_section
+        WHERE f_id IN (
+            SELECT f_id
+            FROM faculty
+            WHERE f_rank <> 'Assistant'
+        )
+    )
+);
+
+-- Q6.
+SELECT s_id
+FROM student
+WHERE s_id NOT IN (
+    SELECT s_id
+    FROM enrollment
+    WHERE c_sec_id IN (
+        SELECT c_sec_id
+        FROM course_section
+        WHERE loc_id IN (
+            SELECT loc_id
+            FROM location
+            WHERE capacity >= 100
+        )
+    )
+);
+
+-- Q7.
+SELECT s_id
+FROM student
+WHERE s_id IN (
+    SELECT s_id
+    FROM enrollment
+    WHERE c_sec_id IN (
+        SELECT c_sec_id
+        FROM course_section
+        WHERE f_id = (
+            SELECT f_id
+            FROM course_id
+            WHERE c_sec_id = 5;
+        )
+    )
+);
